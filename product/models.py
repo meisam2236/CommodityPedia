08f5e1f40c5e09e6d98f9253f339bd0cada7bbc9
ehsan <ehsankhorram72@gmail.com>
2022-05-05 01:57:59 +0430

    fixed models

--- product/models.py
+++ product/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    icon = models.CharField(max_length=255, null=False)
+
+
 class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    main_image = models.CharField(max_length=255)
+    category_id = models.IntegerField(default=1)
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField(null=True)
+    main_image = models.CharField(max_length=255, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user_id = models.IntegerField()
-    category_id = models.IntegerField()
 
 
-class Category(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    icon = models.CharField(max_length=255)
+class Owner(models.Model):
+    user_id = models.IntegerField(null=False)
+    product_id = models.IntegerField(null=False)
+    price = models.IntegerField(null=False)
+
--- user/models.py
+++ user/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    profile_pic = models.CharField(max_length=255)
+    profile_pic = models.CharField(max_length=255, default="images/profile_pic")
     is_marketer = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=11, null=False)
