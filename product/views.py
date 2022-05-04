26e0f2ea17d5eb6208af2fa35650c3ecbecad46b
Meisam Taghizadeh <meisam2236@gmail.com>
2022-05-04 16:53:40 +0430

    Added login capability with drf!

--- product/views.py
+++ product/views.py
@@ -1,6 +1,5 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from product.models import *
 from product.serializers import ProductSerializer
--- user/views.py
+++ user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as dj_login
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+@csrf_exempt
+@api_view(['POST'])
+def login(request):
+    user = UserSerializer(data=request.data).initial_data
+    user = authenticate(request, username=user.get("username"), password=user.get("password"))
+    if user:
+        dj_login(request, user)
+        return Response({'success': True})
+    return Response({'success': False})
